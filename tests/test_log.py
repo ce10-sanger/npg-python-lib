3ed7e27c99a35a73a683738efa57272b468d36b8
Calum Eadie <199819990+ce10-sanger@users.noreply.github.com>
2026-09-23 14:11:28 +0100

    WIP

--- tests/test_log.py
+++ tests/test_log.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import json
 import logging
+from contextlib import contextmanager
 from pathlib import Path
 
 import structlog
@@ -29,11 +30,12 @@ from npg.log import configure_structlog
 class TestConfigureStructlog:
     @m.context("When configuring with default options")
     @m.it("Logs to stderr")
-    def test_normal_defaults(self, reset_logging, capsys: CaptureFixture):
+    def test_normal_defaults(self, capsys: CaptureFixture):
         # Act
-        configure_structlog()
-        log = structlog.stdlib.get_logger()
-        log.error("Test Message")
+        with reset_logging():
+            configure_structlog()
+            log = structlog.stdlib.get_logger()
+            log.error("Test Message")
 
         # Assert
         stderr = capsys.readouterr().err
@@ -42,9 +44,7 @@ class TestConfigureStructlog:
 
     @m.context("When configuring with config file specifying logging to file")
     @m.it("Logs to file")
-    def test_normal_config_file(
-        self, reset_logging, capsys: CaptureFixture, tmp_path: Path
-    ):
+    def test_normal_config_file(self, capsys: CaptureFixture, tmp_path: Path):
         # Arrange
         config_file = tmp_path / "logging.json"
         log_path = tmp_path / "test.log"
@@ -76,9 +76,10 @@ class TestConfigureStructlog:
         )
 
         # Act
-        configure_structlog(config_file)
-        log = structlog.stdlib.get_logger()
-        log.error("Test Message")
+        with reset_logging():
+            configure_structlog(config_file)
+            log = structlog.stdlib.get_logger()
+            log.error("Test Message")
 
         # Assert
         stderr = capsys.readouterr().err
@@ -90,13 +91,56 @@ class TestConfigureStructlog:
     @m.context("When configuring with missing config file")
     @m.it("Falls back to default log to stderr behaviour")
     @m.it("and logs a CRITICAL error to stderr")
-    def test_error(self, reset_logging, capsys: CaptureFixture):
+    def test_error(self, capsys: CaptureFixture):
         # Act
-        configure_structlog("missing-logging.json")
-        log = structlog.stdlib.get_logger()
-        log.error("Test Message")
+        with reset_logging():
+            configure_structlog("missing-logging.json")
+            log = structlog.stdlib.get_logger()
+            log.error("Test Message")
 
         # Assert
         stderr = capsys.readouterr().err
         assert "Test Message" in stderr
         assert "CRITICAL" in stderr
+
+
+@contextmanager
+def reset_logging():
+    """
+    Resets logging before and after each test to isolate a test from pytest
+    logging plugin and other tests.
+
+    Logging has global state which is modified by pytest logging plugin and
+    may be modified by a test.
+
+    logging.basicConfig does nothing if root logger already has handlers configured.
+
+    Disables caplog functionality.
+
+    pytest installs its per-test logging handlers after fixture setup, so
+    logging must be reset from within the test call itself hence in test context
+    manager rather than fixture.
+    """
+
+    # Remove pytest logging plugin
+    root = logging.getLogger()
+    pytest_handlers = root.handlers[:]
+    original_level = root.level
+
+    root.handlers.clear()
+    structlog.reset_defaults()
+
+    try:
+        yield
+
+    finally:
+        # Remove test handlers
+        for handler in root.handlers[:]:
+            root.removeHandler(handler)
+            if handler not in pytest_handlers:
+                handler.close()
+
+        # Restore pytest logging plugin
+        root.handlers[:] = pytest_handlers
+        root.setLevel(original_level)
+        structlog.reset_defaults()
